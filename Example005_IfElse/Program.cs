5541f1f3ef5244bad8a279fe27b82ecc18f4f9b8
vanyusha82228 <ivan00.02@mail.ru>
2022-10-12 00:07:32 +0300

    add if

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "полина")
+{
+    Console.WriteLine("Ура, этож моя любофь- ПОлина!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
