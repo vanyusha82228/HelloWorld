47e37dc1ae3644621f4cfa9c2baf7b836f6e44f6
vanyusha82228 <ivan00.02@mail.ru>
2022-10-11 23:46:08 +0300

    random

--- Exammple003_Sum/Program.cs
+++ Exammple003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 33;
-int numberB = 52;
+﻿int numberA = new Random().Next(1,10); // 1,2,3,4...9 
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result); 
